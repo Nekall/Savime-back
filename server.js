4f46c3e74cd649e268ed3c8af9d739161cb5713f
Neka <nekoanchor@yahoo.fr>
2023-02-03 13:55:11 +0100

    refacto: rename env var

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.MYSQLHOST,
     dialect: 'mysql',
     logging: false,
   }
